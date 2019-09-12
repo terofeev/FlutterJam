0f5eaf2b90abb0cd020985cd38f8f9b10bc64568
Tim <tim@all.me>
2019-09-12 22:12:01 +0300

    Add flutter project files.

---
+++ android/app/src/main/kotlin/com/terofeev/flutter_jam/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.terofeev.flutter_jam
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
